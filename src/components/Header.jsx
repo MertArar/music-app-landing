53288f795aaf4fa8d91b79a3d9edbdb610ec9bc6
MertArar <85193329+MertArar@users.noreply.github.com>
2023-04-11 02:26:19 +0300

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,24 @@ function Header() {
   const buttonStyle =
     "border-[2px] rounded-[10px] border-[#232a4e] px-[25px] py-[7px]";
   return (
-    <div className=" ">
+    <div className="header bg-[#081730] flex items-center justify-between px-[5rem] pt-[2.4rem] text-[0.8rem]">
+      {/* logo */}
+      <img
+        src={require("../img/MuzicLogo.png")}
+        alt=""
+        className="logo w-[42px] h-[42px]"
+      />
       {/* side menu */}
-      <img />
       <CenterMenu />
-      <div>{/* side menu */}</div>
+      {/* buttons */}
+      <div className="buttons flex">
+        <button className={`mr-[35px] hover:bg-[#232a4e] ` + buttonStyle}>
+          Log in
+        </button>
+        <button className={buttonStyle + `mr-[35px] hover:bg-[#232a4e]`}>
+          Sign Up
+        </button>
+      </div>
     </div>
   );
 }
