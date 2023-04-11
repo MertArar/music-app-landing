7b7a22c641da974c9e6d84bf6e76387880e4971a
MertArar <85193329+MertArar@users.noreply.github.com>
2023-04-11 03:55:38 +0300

    Experience and Features added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Header from "./components/Header";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div className="App text-white">
       <Header />
       <Hero />
+      <Experience />
     </div>
   );
 }
